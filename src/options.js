a8ef15a8f8008bb61ee33e780a443dfe67232eba
akmjenkins <akmjenkins@gmail.com>
2021-09-13 19:58:45 -0300

    add/replace

--- src/options.js
+++ src/options.js
@@ -2,8 +2,5 @@ import { get } from './pointer';
 import { patch } from './patch';
 const pattern = /\{\{\s*(.+?)\s*\}\}/g;
 const resolver = get;
-const validator = () => {
-  throw new Error('You must supply a validator');
-};
 
-export default { pattern, resolver, patch, validator };
+export default { pattern, resolver, patch };
--- test/patch.test.ts
+++ test/patch.test.ts
@@ -54,6 +54,36 @@ describe('patch', () => {
     expect(newDoc.h.i.m?.l).toBe(2);
   });
 
+  it('should replace', () => {
+    type Patch = { m: number };
+    const newDoc = patch<Doc & Patch>(doc, [
+      { op: 'replace', path: '/m', value: 2 },
+    ]);
+    expect(newDoc.m).toBe(2);
+    // immutable
+    expect(newDoc).not.toBe(doc);
+  });
+
+  it('should replace nested (array)', () => {
+    type Patch = { m?: { l: number } };
+    type PatchedI = { i: Doc['h']['i'] & Patch };
+    type PatchedH = { h: Doc['h'] & PatchedI };
+    const newDoc = patch<Doc & PatchedH>(doc, [
+      { op: 'replace', path: '/h/i/m/l/-', value: 2 },
+    ]);
+    expect(newDoc.h.i.m?.l).toEqual([2]);
+  });
+
+  it('should replace nested (object)', () => {
+    type Patch = { m?: { l: number } };
+    type PatchedI = { i: Doc['h']['i'] & Patch };
+    type PatchedH = { h: Doc['h'] & PatchedI };
+    const newDoc = patch<Doc & PatchedH>(doc, [
+      { op: 'replace', path: '/h/i/m/l', value: 2 },
+    ]);
+    expect(newDoc.h.i.m?.l).toBe(2);
+  });
+
   it('should append', () => {
     expect(
       patch(doc, [{ op: 'add', path: '~1h/i/j~1-', value: 90 }]).h.i.j[2],
@@ -70,8 +100,6 @@ describe('patch', () => {
     expect(newDoc.b.pop()).toBe(doc.b[0].d); // should be the original
     expect(newDoc.b[0]).not.toHaveProperty('d'); // moved
   });
-
-  it('should replace', () => {});
 
   it('should test', () => {
     expect(
