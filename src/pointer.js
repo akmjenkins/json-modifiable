ff24a5c1b157347947ee86962cfc9ed12ea327ee
akmjenkins <akmjenkins@gmail.com>
2021-09-13 19:31:21 -0300

    Fix json patch logic

--- src/pointer.js
+++ src/pointer.js
@@ -19,6 +19,8 @@ const shallowClone = (thing) =>
     ? { ...thing }
     : thing;
 
+const isNumber = (n) => n < Infinity;
+
 const _getPointerObj = (object, pointer) => {
   const compiled = compile(pointer);
   const last = compiled.pop();
@@ -26,7 +28,7 @@ const _getPointerObj = (object, pointer) => {
   const lastObject = compiled.reduce((acc, piece, i) => {
     if (typeof acc[piece] === 'undefined') {
       const nextPiece = compiled[i + 1] || last;
-      return (acc[piece] = nextPiece < Infinity || nextPiece === '-' ? [] : {});
+      return (acc[piece] = isNumber(nextPiece) || nextPiece === '-' ? [] : {});
     }
     return (acc[piece] = shallowClone(acc[piece]));
   }, next);
@@ -42,7 +44,13 @@ export const set = (object, pointer, value) => {
   const { last, next, lastObject } = _getPointerObj(object, pointer);
   // no mutations necessary, return the same reference
   if (lastObject[last] === value) return object;
-  last === '-' ? lastObject.push(value) : (lastObject[last] = value);
+  if (isNumber(last) && Array.isArray(lastObject)) {
+    lastObject.splice(last, 0, value);
+  } else if (last === '-') {
+    lastObject.push(value);
+  } else {
+    lastObject[last] = value;
+  }
   return next;
 };
 
@@ -54,6 +62,7 @@ export const unset = (object, pointer) => {
 
   if (last === '-') lastObject.pop();
   if (typeof lastObject[last] === 'undefined') return object;
+
   Array.isArray(lastObject)
     ? lastObject.splice(last, 1)
     : delete lastObject[last];
